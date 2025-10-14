5515aace9bc2c4193b30921171036446462bd747
Lance Negrut <lance@MacBook-Air-749.local>
2025-10-14 12:23:48 -0400

    Bring out the functions

--- adelie_penguins.py
+++ adelie_penguins.py
@@ -2,3 +2,30 @@
 # Student ID: 70298465
 # Email: lnegrut@umich.edu
 # Collaborators: Gemini (I’m not working with other classmates on this project)
+
+def load_data():
+    pass
+
+def only_adelie(data):
+    pass
+
+def find_min_mass(adelie):
+    pass
+
+def find_max_mass(adelie):
+    pass
+
+def calculate_range(adelie):
+    pass
+
+def this_island_only(adelie, island):
+    pass
+
+def calculate_average(adelie):
+    pass
+
+def generate_findings(min_mass, max_mass, range_mass, average_mass):
+    pass
+
+def main():
+    pass
