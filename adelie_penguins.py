25621a732c79893c1be5614aeef23dc990d25712
Lance Negrut <lance@macbook-air-749.lan>
2025-10-14 17:36:15 -0400

    def specifics

--- adelie_penguins.py
+++ adelie_penguins.py
@@ -6,20 +6,42 @@
 import csv
 import unittest
 
-def load_data(csv_file):
-    pass  
+def load_data(inFile):
+    data = []
+    with open(inFile, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            for key in ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']:
+                if row[key] and row[key] != 'NA':
+                    row[key] = float(row[key])
+                else:
+                    row[key] = None
+            data.append(row)
+    return data  
 
 def specific_species(data, species):
-    pass
+    specific_species_ls = []
+    for penguin in data:
+        if penguin['species'] == species:
+            specific_species_ls.append(penguin)
+    return specific_species_ls
 
 def specific_island(data, island):
-    pass
+    specific_island_ls = []
+    for penguin in data:
+        if penguin['island'] == island:
+            specific_island_ls.append(penguin)
+    return specific_island_ls
 
 def calculate_averages(data):
     pass
 
 def specific_sex(data, sex):
-    pass
+    specific_sex_ls = []
+    for penguin in data:
+        if penguin['sex'] == sex:
+            specific_sex_ls.append(penguin)
+    return specific_sex_ls
 
 def find_min(data, column):
     pass
@@ -31,7 +53,17 @@ def calculate_ranges(data):
     pass
 
 def generate_findings(averages, ranges):
-    pass
+    with open('adelie_analysis.txt', 'w') as f:
+        f.write("Penguin Analysis Report\n")
+        f.write("========================\n\n")
+        
+        f.write("Average Body Mass (g) by Species:\n")
+        for species, avg_mass in averages.items():
+            f.write(f"- {species}: {avg_mass}g\n")
+            
+        f.write("\nBill Length Range (mm) by Island:\n")
+        for island, range_data in ranges.items():
+            f.write(f"- {island}: {range_data['range']}mm (Min: {range_data['min']}, Max: {range_data['max']})\n")
 
 class myTests(unittest.TestCase):
     def setUP(self):
@@ -71,8 +103,10 @@ class myTests(unittest.TestCase):
         pass
 
 def main():
-    inFile = open("penguins.csv")
-    csv_file = csv.reader(inFile)
+    data = load_data("/Users/lance/Desktop/SI201/fall25-project1-LanceNegrut/penguins.csv")
+    averages = calculate_averages(data)
+    ranges = calculate_ranges(data)
+    generate_findings(averages, ranges)
     unittest.main(verbosity=2)
 
 if __name__ == "__main__":
