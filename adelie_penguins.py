9e47febbe05de89331f862cbdaef9ca34540637f
Lance Negrut <lance@MacBook-Air-749.local>
2025-10-14 12:47:45 -0400

    Possibly the functions for the test cases

--- adelie_penguins.py
+++ adelie_penguins.py
@@ -3,8 +3,8 @@
 # Email: lnegrut@umich.edu
 # Collaborators: Gemini (I’m not working with other classmates on this project)
 
-def load_data():
-    pass
+def load_data(csv_file):
+    pass  
 
 def only_adelie(data):
     pass
@@ -27,5 +27,40 @@ def calculate_average(adelie):
 def generate_findings(min_mass, max_mass, range_mass, average_mass):
     pass
 
+import unittest
+
+class myTests(unittest.TestCase):
+    def test_load_data(self):
+        pass
+    
+    def test_only_adelie(self):
+        pass
+
+    def test_find_min_mass(self):
+        pass
+
+    def test_find_max_mass(self):
+        pass
+
+    def test_calculate_range(self):
+        pass
+
+    def test_this_island_only(self):
+        pass
+
+    def test_calculate_average(self):
+        pass
+
+    def test_generate_findings(self):
+        pass
+
+    def test_main(self):
+        pass
+
 def main():
-    pass
+    import csv
+    inFile = open("penguins.csv")
+    csv_file = csv.reader(inFile)
+
+if __name__ == "__main__":
+    main()
